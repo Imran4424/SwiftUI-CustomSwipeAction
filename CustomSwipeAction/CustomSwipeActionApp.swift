5b4ecf8a253afa1ecaf394944e66c962b1b206fe
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2024-02-17 21:27:34 +0600

    project init

---
+++ CustomSwipeAction/CustomSwipeActionApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSwipeActionApp.swift
+//  CustomSwipeAction
+//
+//  Created by Shah Md Imran Hossain on 17/2/24.
+//
+
+import SwiftUI
+
+@main
+struct CustomSwipeActionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
